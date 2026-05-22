2fc0f0f866e5dc565c1526b7b9b2cb887ef410f2
vishal <vishal.yadav@myfindoc.com>
2026-05-22 13:00:49 +0530

    test - 6

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get("/adding-test",(req,res)=>{
         status: "ok",
         time: new Date().toISOString()
     });
-    res.write("Install the playwrite test dependency test - 5");
+    res.write("Install the playwrite test dependency test - 6");
 })
 
 app.get("/check-ios-update", checkUpdate);
